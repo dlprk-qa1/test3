72da0588dc21dbd239ce506182af865d693634a0
dlprk-qa1 <97515913+dlprk-qa1@users.noreply.github.com>
2022-03-04 20:00:14 +0530

    Create main.tf

---
+++ ec2/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.11"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+module "storage" {
+  source = "./modules/storage"
+}
